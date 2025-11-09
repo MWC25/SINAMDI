ddcc4e9e2979cf43379dffe4813fa6ac886b2741
caio <nazzariocaio@gmail.com>
2025-11-09 16:06:29 -0300

    🔧chore: ajuste admin

--- back-end/sinamdi_core/api/admin.py
+++ back-end/sinamdi_core/api/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import (
+    AlgoritmosHash,
+    StatusInstituicao,
+    StatusCaso,
+    RiscoNivel,
+    Roles,
+    TiposMidia,
+    PublicosAlvo,
+    FaixasEtarias,
+    Generos,
+    QuestoesAutoavaliacao,
+    Estados,
+    Municipios,
+    Instituicoes,
+    Usuarios,
+    CasosInstitucionais,
+    AutoAvaliacoes,
+    MateriaisEducativos,
+    UsuariosInstituicoes,
+    AutoAvaliacoesRespostas,
+    Auditoria,
+    VwCasosAtivos,
+    VwEstatisticasInstituicao,
+)
 
-# Register your models here.
+# Registrar todas as models no admin
+models = [
+    AlgoritmosHash,
+    StatusInstituicao,
+    StatusCaso,
+    RiscoNivel,
+    Roles,
+    TiposMidia,
+    PublicosAlvo,
+    FaixasEtarias,
+    Generos,
+    QuestoesAutoavaliacao,
+    Estados,
+    Municipios,
+    Instituicoes,
+    Usuarios,
+    CasosInstitucionais,
+    AutoAvaliacoes,
+    MateriaisEducativos,
+    UsuariosInstituicoes,
+    AutoAvaliacoesRespostas,
+    Auditoria,
+    VwCasosAtivos,
+    VwEstatisticasInstituicao,
+]
+
+for model in models:
+    admin.site.register(model)
