a1dc8be8132785bd9ad84282c4597b62f4ac87b1
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-11-16 08:54:17 -0300

    feat: adiciona serviços de autenticação e administração de usuários

---
+++ back-end/src/services/admin.service.ts
@@ -0,0 +1,25 @@
+import { userDTO } from "../DTOs/user.dto"
+import { User } from "../generated/prisma/client";
+import { AdminRepository } from "../repositories/admin.repository";
+import { hashPassword } from "../util/cryptPassword";
+import { generateRegistration } from "../util/generateResitration";
+
+export const AdminService = {
+    async createAdminUser(data: any): Promise<User>  {
+        try {
+            const dto = userDTO.buildCreateUserDto(data);
+            const hashedPassword = await hashPassword(dto.password);
+
+            const newAdmin = await AdminRepository.createAdmin({
+                username: dto.username,
+                passwordHash: hashedPassword,
+                registration: generateRegistration(),
+                role: 'ADMIN',
+            });
+
+            return newAdmin;
+        } catch (error: any) {
+            throw new Error(`Failed to create admin user: ${error.message}`);
+        }
+    }
+}
---
+++ back-end/src/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { userRepository } from "../repositories/user.repository"
+import { verifyPassword } from "../util/cryptPassword";
+import 'dotenv/config'
+import { JWTProvider } from "../config/JWTProvider";
+
+export const authService = {
+    async authenticate(req: Request, res: Response){
+
+        const {username, password}= req.body;        
+
+        const user = await userRepository.getUser(username)
+
+        if (!user){
+            return res.status(401).json({
+                message: "Invalid Credentials"
+            })
+        }
+
+        if (await verifyPassword(password, user.passwordHash) === false){
+            return res.status(401).json({
+                message: 'Invalid Credentials',
+            });
+        }
+
+        const token = JWTProvider.generateToken(user.id);
+
+        return res.status(200).json({
+                message: 'Login successful',
+                accessToken: token,
+                user: user,
+            });
+    }
+}
