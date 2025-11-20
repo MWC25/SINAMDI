8fe04cc53e7c6d0ec96e8e31783c8d97674e234e
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-11-20 10:28:09 -0300

    fix: corrige o bug da url do database do prisma7

--- back-end/src/config/env.ts
+++ back-end/src/config/env.ts
@@ -2,3 +2,4 @@ import * as jwt from "jsonwebtoken";
 
 export const PORT = process.env.PORT || 8080;
 export const JWTSECRET: jwt.Secret = process.env.JWT_SECRET!;
+export const DATABASE_URL = process.env.DATABASE_URL!;
