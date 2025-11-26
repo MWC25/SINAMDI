c6288c6e43fbf900f3b509bc51eda08bae2c1e1f
caio <nazzariocaio@gmail.com>
2025-11-26 18:51:45 -0300

    feat: cria o dockerfile e corrige o axios.config

--- front-end/src/services/axios.config.ts
+++ front-end/src/services/axios.config.ts
@@ -1,8 +1,13 @@
+// front-end/src/services/axios.config.ts
 import axios from 'axios';
+
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080';
+
 export const client = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true,
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
 });
