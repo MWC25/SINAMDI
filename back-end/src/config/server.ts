ed5ec0ac71d9f60f676bcba533887db668148d30
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-12-11 23:29:04 -0300

    fix: ajusta nomes de variáveis de ambiente para consistência no backend

--- back-end/src/config/server.ts
+++ back-end/src/config/server.ts
@@ -1,11 +1,11 @@
 import api from '../api';
 import { createAdminAuto } from '../util/createAdminAuto';
-import { PORT, URL_SERVER } from './env';
+import { PORT, URLSERVER } from './env';
 import 'dotenv/config.js';
 import { logger } from './logger';
 
-api.listen(PORT, () => {
-    logger.info(`Server is running on port http://${URL_SERVER}:${PORT}`);
-    logger.info(`Server docs is running on port http://${URL_SERVER}:${PORT}/api/doc`);
+api.listen(Number(PORT), URLSERVER, () => {
+    logger.info(`Server is running on port http://${URLSERVER}:${PORT}`);
+    logger.info(`Server docs is running on port http://${URLSERVER}:${PORT}/api/doc`);
     createAdminAuto();
 });
