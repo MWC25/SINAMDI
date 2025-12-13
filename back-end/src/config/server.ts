80fb4312ff4c7a9102587c2e7a9832db6fbcc036
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-12-13 10:28:54 -0300

    deploy

--- back-end/src/config/server.ts
+++ back-end/src/config/server.ts
@@ -1,11 +1,10 @@
 import api from '../api';
 import { createAdminAuto } from '../util/createAdminAuto';
-import { PORT, URLSERVER } from './env';
 import 'dotenv/config.js';
 import { logger } from './logger';
 
-api.listen(Number(PORT), URLSERVER, () => {
-    logger.info(`Server is running on port http://${URLSERVER}:${PORT}`);
-    logger.info(`Server docs is running on port http://${URLSERVER}:${PORT}/api/doc`);
+api.listen(8080, "0.0.0.0", () => {
+    logger.info(`Server is running on port http://0.0.0.0:8080`);
+    logger.info(`Server docs is running on port http://0.0.0.0:8080/api/doc`);
     createAdminAuto();
 });
